c367f976f37d888027accb8811761363471ed7a9
Sander C. Braathen <sanderbraathen67@gmail.com>
2024-10-11 11:21:17 +0200

    Update multiplayer_test.gd

--- multiplayer_test.gd
+++ multiplayer_test.gd
@@ -1,17 +1,21 @@
+# multiplayer_test.gd
 extends Node2D
 
 var peer = ENetMultiplayerPeer.new()
 @export var player_scene: PackedScene
-@onready var ip_address_input: LineEdit = $LineEdit  # Assuming you've added a LineEdit node named IPAddressInput
+@export var projectile_scene: PackedScene
+@onready var ip_address_input: LineEdit = $LineEdit
 
 func _ready():
 	multiplayer.connected_to_server.connect(_on_connected_to_server)
+	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
 
 func _on_host_pressed() -> void:
 	var error = peer.create_server(135)
 	if error == OK:
 		multiplayer.multiplayer_peer = peer
 		multiplayer.peer_connected.connect(_add_player)
+		multiplayer.peer_disconnected.connect(_remove_player)
 		_add_player()
 	else:
 		print("Failed to create server: ", error)
@@ -21,10 +25,15 @@ func _add_player(id = 1):
 	player.name = str(id)
 	call_deferred("add_child", player)
 
+func _remove_player(id):
+	if has_node(str(id)):
+		var player = get_node(str(id))
+		player.queue_free()
+
 func _on_join_pressed() -> void:
 	var ip = ip_address_input.text.strip_edges()
 	if ip.is_empty():
-		ip = "localhost"  # Default to localhost if no IP is entered
+		ip = "localhost"
 	
 	var error = peer.create_client(ip, 135)
 	if error == OK:
@@ -34,3 +43,20 @@ func _on_join_pressed() -> void:
 
 func _on_connected_to_server():
 	_add_player(multiplayer.get_unique_id())
+
+func _on_peer_disconnected(id):
+	_remove_player(id)
+
+func _notification(what):
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		_handle_exit()
+
+func _handle_exit():
+	if multiplayer.is_server():
+		for id in multiplayer.get_peers():
+			_remove_player(id)
+	multiplayer.multiplayer_peer = null
+	get_tree().quit()
+
+func get_projectile_scene() -> PackedScene:
+	return projectile_scene
